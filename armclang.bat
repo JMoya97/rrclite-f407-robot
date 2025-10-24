6786b84730f8a1e54c0aa0305d1b36671dcb8f2e
JMoya97 <jmoya9718@gmail.com>
2025-10-24 20:31:43 +0200

    delete png and keil files

--- armclang.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-set "source_path=.\Middlewares\Third_Party\FreeRTOS\Source\portable\GCC"
-set "destination_path=.\Middlewares\Third_Party\FreeRTOS\Source\portable\RVDS"
-
-xcopy /E /I /Y "%source_path%" "%destination_path%"
-
-choice /t 1 /d y /n >nul
